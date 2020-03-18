515b0c8e86cb8777bb187b81ae4c1dcbcb463603
USER <>
2020-03-18 14:24:04 +0900

    no message

---
+++ KittyTest/KittyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KittyTest
+//
+//  Created by USER on 2020/03/18.
+//  Copyright © 2020 Igarashi Ai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
